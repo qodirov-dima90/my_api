2d3ad794b546562dfc7757ee40d91dbcafd67e8c
Qodirov Dilmurod <dimakpservis90@gmail.com>
2022-10-23 12:46:50 +0500

    add login action to authenticate a user (commit)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,19 @@ class UsersController < ApplicationController
             render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
     end
 end
+    def login 
+        @user - User.find_by(username: user_params[:username])
+
+        if @user && @user.authenticate(user_params[:password])
+            token = encode_token({ user_id: @user.id})
+            render json: { user: @user, token: token}, status: :ok
+        else
+            render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+        end
+    end
 
 private 
+
     def user_params
         params.require(:user).permit(:username, :password)
     end
