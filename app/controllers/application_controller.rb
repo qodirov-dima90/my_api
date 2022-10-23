cf87fc6302068ba867a6e160555c615bb8e564eb
Qodirov Dilmurod <dimakpservis90@gmail.com>
2022-10-23 21:27:33 +0500

    Add authorize method into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,8 @@ class ApplicationController < ActionController::API
             @user = User.find_by(id: user_id)
         end
     end
+    def authorize 
+        render json: { message: 'Your hove to log in.'}, status: :unauthorized unless authorized_user
+    end
+            
 end
